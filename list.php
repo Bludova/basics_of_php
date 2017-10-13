0b6cee7e4a990a6b04b9a9df4443328c4a1a67e2
Bludova <32148601+Bludova@users.noreply.github.com>
2017-10-13 15:00:05 +0300

    Update list.php

--- list.php
+++ list.php
@@ -1,23 +1,59 @@
-<?php
-//header('Location: ./list.php',TRUE,302);
-header("Location: ./list.php",TRUE,302);
-?>
 <!DOCTYPE html>
   <html lang="ru">
   <head>
     <meta charset="UTF-8">
-    <title>Обработка форм</title>
+    <title>Список загруженных тестов</title>
   </head>
   <body>
-<h1>Загрузить JSON-файл c тестом.</h1>
+<h1>Список загруженных тестов</h1>
 
 
-<form enctype="multipart/form-data" action="./list.php" method="POST">
-Загрузить JSON-файл <input name="userfile" type="file">
-<br>
-<input type="submit" value="Отправить">
-</form>
+      <form enctype="multipart/form-data" action="test.php" method="get">
+          <legend>Список загруженных тестов</legend>
+            <?php 
+
+$number = 0;
+$filelist = glob("*.json");
+ 
+foreach($filelist as $i => $filename) {
+   $number++;
+    $name = basename($filename);
+
+?>
+
+
+          <label><input type="radio" name='<?="$number"?>' value='<?="$filename"?>'>
+
+        <?= " № $number" . $filename;?>
+         <?php
+}
+?>
+        </label>
+        <input type="submit" value="Пройти тест!">
+      </form>
+
+
+<?php
+// $files_test = $_FILES['userfile'] ['name'];
+?> 
+
+<?php
+if(isset($_FILES['userfile'] ['name']) && !empty($_FILES['userfile'] ['name']))
+{
+  if($_FILES['userfile'] ['error'] == UPLOAD_ERR_OK &&
+    move_uploaded_file($_FILES['userfile'] ['tmp_name'], $_FILES['userfile'] ['name']))
+  {
+    echo "Файл с текстом загружен! <br>";
+  }else 
+  {
+    echo " Ошибка: Файл с текстом не загружен! <br>";
+
+  }
+}
+
+?>
+
 <hr>
-<a href="./list.php">Выбрать тест! </a>
+<a href="admin.php">Загрузить тест!</a>
   </body>
 </html>
