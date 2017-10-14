7bb2b2d634848fcdd3ddceb13f8930750ee67e11
Bludova <32148601+Bludova@users.noreply.github.com>
2017-10-14 22:05:05 +0300

    Update list.php

--- list.php
+++ list.php
@@ -5,55 +5,39 @@
     <title>Список загруженных тестов</title>
   </head>
   <body>
-<h1>Список загруженных тестов</h1>
-
-
-      <form enctype="multipart/form-data" action="test.php" method="get">
-          <legend>Список загруженных тестов</legend>
-            <?php 
-
-$number = 0;
-$filelist = glob("*.json");
- 
-foreach($filelist as $i => $filename) {
-   $number++;
-    $name = basename($filename);
-
-?>
-
-
-          <label><input type="radio" name='<?="$number"?>' value='<?="$filename"?>'>
-
-        <?= " № $number" . $filename;?>
-         <?php
-}
-?>
+    <h1>Список загруженных тестов</h1>
+      <form enctype="multipart/form-data" action="./test.php" method="get">
+        <legend>Список загруженных тестов</legend>
+        <?php 
+          $filelist = glob("*.json");
+          foreach($filelist as $i => $filename) {
+          ++$i;
+          $name = basename($filename);
+        ?>
+        <label><input type="radio" name='<?="$i"?>' value='<?="$filename"?>' >
+          <?php echo  "№$i-"." ". $filename ;
+          }
+          ?>
         </label>
         <input type="submit" value="Пройти тест!">
       </form>
 
+      <?php
+      if(isset($_FILES['userfile'] ['name']) && !empty($_FILES['userfile'] ['name']))
+      {
+      if($_FILES['userfile'] ['error'] == UPLOAD_ERR_OK &&
+        move_uploaded_file($_FILES['userfile'] ['tmp_name'], $_FILES['userfile'] ['name']))
+      {
+        echo "Файл с текстом загружен! <br>";
+      }else 
+        {
+          echo " Ошибка: Файл с текстом не загружен! <br>";
 
-<?php
-// $files_test = $_FILES['userfile'] ['name'];
-?> 
+       }
+     }
+      ?>
 
-<?php
-if(isset($_FILES['userfile'] ['name']) && !empty($_FILES['userfile'] ['name']))
-{
-  if($_FILES['userfile'] ['error'] == UPLOAD_ERR_OK &&
-    move_uploaded_file($_FILES['userfile'] ['tmp_name'], $_FILES['userfile'] ['name']))
-  {
-    echo "Файл с текстом загружен! <br>";
-  }else 
-  {
-    echo " Ошибка: Файл с текстом не загружен! <br>";
-
-  }
-}
-
-?>
-
-<hr>
-<a href="admin.php">Загрузить тест!</a>
+    <hr>
+    <a href="./admin.php">Загрузить тест!</a>
   </body>
 </html>
