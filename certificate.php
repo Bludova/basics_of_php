27d0dec64b00f751a9fe7265db6d2f63609abeaa
Bludova <32148601+Bludova@users.noreply.github.com>
2017-10-23 14:53:53 +0300

    Add files via upload

--- certificate.php
+++ certificate.php
@@ -1,17 +1,18 @@
 <?php
 $code = $_GET['name'];
 $evaluation = ' Вы справились с тестом!';
+
 $image = imagecreatetruecolor(600, 412);
 $backColor = imagecolorallocate($image,  255, 221, 0);
 $textColor = imagecolorallocate($image, 0, 133, 11);
 
-$fontFile = __DIR__. '/roboto_black.ttf';
+$fontFile = __DIR__. './roboto_black.ttf';
 if (!file_exists($fontFile)) {
   echo 'Файл со шрифтом не найден!';
   exit;
 }
 
-$imBox = imagecreatefrompng( 'certificate.png');
+$imBox = imagecreatefrompng( __DIR__. './image/certificate.png');
 
 imagefill($image, 0, 0, $backColor);
 imagecopy($image, $imBox, 10, 10, 0, 0, 578, 392);
